e985e7c2149a348ac4f5118639180b233de91873
Scott Geleas <scottgeleas97@gmail.com>
2021-08-11 22:13:48 -0400

    added screenshots

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
